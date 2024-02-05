7569519da9887c5fb3f13b44d4f3b48c57396ba2
Gaurav Singh <145004286+ArRahman79@users.noreply.github.com>
2024-02-05 22:29:55 +0530

    Create Problem no. 387.c
    First unique character in a string.

---
+++ Problem no. 387.c
@@ -0,0 +1,18 @@
+// 05-02-2024
+
+int firstUniqChar(char* s) {
+    int freq[26]={0};
+    for(int i=0;i<strlen(s);i++)
+    {
+        freq[s[i]-'a']++;
+    }
+
+    for(int i=0;i<strlen(s);i++)
+    {
+        if(freq[s[i]-'a']==1)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
